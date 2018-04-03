b77bbf3f480af15dca340ff418ef8ec73bb25d4b
littlemauu <maurichini@gmail.com>
2018-04-03 13:19:00 -0500

    Caso de uso Ver Tema

--- src/main/java/unam/ciencias/is/kafka/controlador/TemaAbierto.java
+++ src/main/java/unam/ciencias/is/kafka/controlador/TemaAbierto.java
@@ -8,12 +8,20 @@
  */
 package unam.ciencias.is.kafka.controlador;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import unam.ciencias.is.kafka.modelo.Tema;
 import unam.ciencias.is.kafka.modelo.TemaDAO;
+import unam.ciencias.is.kafka.modelo.Usuario;
+import unam.ciencias.is.kafka.modelo.Responde;
 
 /**
  *
@@ -23,18 +31,51 @@ import unam.ciencias.is.kafka.modelo.TemaDAO;
 @ManagedBean
 @ViewScoped
 public class TemaAbierto {
-    
+
     private String titulo;
     private Tema tema;
-    
-    public TemaAbierto(){
+    private List<Responde> respuestas;
+    private Usuario autor;
+
+    public TemaAbierto() {
         TemaDAO temadao = new TemaDAO();
-        HttpServletRequest request =
-                (HttpServletRequest) FacesContext.getCurrentInstance().
+        HttpServletRequest request
+                = (HttpServletRequest) FacesContext.getCurrentInstance().
                         getExternalContext().getRequest();
         String temaS = request.getParameter("idt");
-        tema = temadao.select(Integer.parseInt(temaS));
+        int idTema = Integer.parseInt(temaS);
+        tema = temadao.select(idTema);
         titulo = tema.getNombreTema();
+        autor = tema.getUsuario();
+        HashSet<Responde> set = new HashSet<Responde>(tema.getRespondes());
+        ArrayList<Responde> al = new ArrayList<Responde>(set);
+        al.sort(new RespondeComparator());
+        respuestas = (List<Responde>) al;
+    }
+
+    public String rutaDeImagen(Usuario usr) {
+	String ruta = usr.getImagen();
+
+	if (ruta == null)
+	    ruta = "imagenes/usuarios/default.png";
+
+	return ruta;
+    }
+
+    public List<Responde> getRespuestas() {
+        return respuestas;
+    }
+
+    public void setRespuestas(List<Responde> respuestas) {
+        this.respuestas = respuestas;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
     }
 
     public String getTitulo() {
@@ -45,15 +86,21 @@ public class TemaAbierto {
         this.titulo = titulo;
     }
 
-    public Tema geTema() {
+    public Tema getTema() {
         return tema;
     }
 
     public void setTema(Tema tema) {
         this.tema = tema;
     }
-    
-    
-    
-    
+
+}
+
+class RespondeComparator implements Comparator<Responde> {
+
+    @Override
+    public int compare(Responde r1, Responde r2) {
+        return r2.getIdResponde() - r1.getIdResponde();
+    }
+
 }
