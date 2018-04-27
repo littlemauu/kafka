543874d0d9880743ebfd2abfa5a52fafabf11485
Paulo García-Ruíz <ludus_sl@ciencias.unam.mx>
2018-04-27 11:47:20 -0500

    Ya funciona el caso "Eliminar usuario"

---
+++ src/main/java/unam/ciencias/is/kafka/controlador/VerPerfilDeUsuario.java
@@ -0,0 +1,72 @@
+/*
+ * The Bugtles, 2018.
+ * -Aguilar Espíndola Ossiel.
+ * -Barajas Tenorio Efraín.
+ * -García Ramos Mauricio E.
+ * -García Ruíz Paulo.
+ * -Ledesma Granados Roberto A.
+ */
+package unam.ciencias.is.kafka.controlador;
+
+import java.io.File;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.apache.commons.lang.RandomStringUtils;
+import unam.ciencias.is.kafka.modelo.Usuario;
+import unam.ciencias.is.kafka.modelo.UsuarioDAO;
+
+/**
+ *
+ * @author ludus
+ */
+@ManagedBean
+@ViewScoped
+public class VerPerfilDeUsuario {
+    private Usuario usuario;
+    
+    public String eliminarUsuario() {
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        usuario = usuarioDAO.select(28);
+        
+        if ("INACTIVO".equals(usuario.getEstado()) ||
+            "ADM".equals(usuario.getRol())) {
+            FacesContext.getCurrentInstance().addMessage(
+                        null,
+                        new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                                "Imposible eliminar al usuario.",
+                                null));
+            return "";
+            //return "VerPerfilDeUsuarioIH?faces-redirect=true&idu=28";
+        }
+        
+        usuario.setNombre("[Eliminado " + usuario.getIdUsuario() + "]");
+        usuario.setContrasena(RandomStringUtils.randomAlphabetic(16));
+        usuario.setEstado("INACTIVO");
+        String imgAnt = usuario.getImagen();
+        usuario.setImagen("imagenes/usuarios/eliminado.png");
+        usuarioDAO.update(usuario);
+        
+        if (imgAnt != null && !imgAnt.equals("imagenes/usuarios/default.png")) {
+            
+            try {
+                File archivo = new File(imgAnt);
+                archivo.delete();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            
+        }
+        
+        FacesContext.getCurrentInstance().addMessage(
+                        null,
+                        new FacesMessage(FacesMessage.SEVERITY_INFO,
+                                "El usuario fue eliminado exitosamente.",
+                                null));
+        return "";
+        //return "VerPerfilDeUsuarioIH?faces-redirect=true&idu=28";
+    }
+    
+}
