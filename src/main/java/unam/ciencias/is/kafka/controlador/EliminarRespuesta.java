402e892e28f5237f2fd26d26b6a348dc70aa91cd
efrain <efrain@192.168.1.66>
2018-05-17 17:43:40 -0500

    Controlador de EliminarResponde

---
+++ src/main/java/unam/ciencias/is/kafka/controlador/EliminarRespuesta.java
@@ -0,0 +1,27 @@
+/*
+ * The Bugtles, 2018.
+ * -Aguilar Espíndola Ossiel.
+ * -Barajas Tenorio Efraín.
+ * -García Ramos Mauricio E.
+ * -García Ruíz Paulo.
+ * -Ledesma Granados Roberto A.
+ */
+package unam.ciencias.is.kafka.controlador;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import unam.ciencias.is.kafka.modelo.Responde;
+import unam.ciencias.is.kafka.modelo.RespondeDAO;
+
+/**
+ *
+ * @author efrain
+ */
+public class EliminarRespuesta {
+      public String eliminaResponde(int id){
+        RespondeDAO RespondeDAO = new RespondeDAO();
+        Responde responde=RespondeDAO.select(id);
+        RespondeDAO.delete(responde);
+        
+        return "TemaAbiertoIH";
+    
+}
